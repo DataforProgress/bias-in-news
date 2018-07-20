5f3cacd73fed9cd4d15bcd46d10e6e0d9c0f8620
njwfish <njwfish@gmail.com>
2018-07-20 12:39:04 -0400

    Created MySQL database and updated parser to facilitate reading into dataparser, completing #3

--- data/nexis/parser.py
+++ data/nexis/parser.py
@@ -4,11 +4,12 @@ from dateutil import parser
 from collections import OrderedDict
 import csv
 import re
+import pymysql.cursors
 
 
 def split_articles(text):
     # Sometimes there's a random header, hard to deal with, remove anything above first doc
-    pattern = re.compile(r'1 of [0-9]+ DOCUMENTS', re.MULTILINE)
+    pattern = re.compile(r' 1 of [0-9]+ DOCUMENTS', re.MULTILINE)
     text = pattern.split(text)[-1]
     pattern = re.compile(r'[0-9]+ of [0-9]+ DOCUMENTS', re.MULTILINE)
     articles = pattern.split(text)
@@ -24,14 +25,13 @@ def get_date(article_lines):
         except ValueError:
             continue
         break
-    return i, d
+    return OrderedDict([('DATE:', {'idx': i, 'val': d})])
 
 
 def get_field_idx_val(i_init, article_lines, fields):
     field_dict = OrderedDict([(field, {'idx': len(article_lines), 'val': None}) for field in fields])
     for idx, line in enumerate(article_lines[i_init:]):
         check = [field for field in fields if field in line]
-        assert len(check) == 0 or len(check) == 1, "Each line should contain a maximum of one field"
         if len(check) == 1:
             field_dict[check[0]]['idx'] = i_init + idx
             field_dict[check[0]]['val'] = line.split(check[0])[-1]
@@ -51,8 +51,9 @@ def parse_pre_meta(i_date, article_lines):
     i_headline = min(field_dict.values(), key=lambda field: field['idx'] - 2)['idx']
     headline = article_lines[i_headline]
     i_pre_meta = max(field_dict.values(), key=lambda field: field['idx'] if field['idx'] != len(article_lines) else -1)['idx']
-    field_dict.update({'HEADLINE': {'idx': i_headline, 'val': headline}})
+    field_dict.update({'HEADLINE:': {'idx': i_headline, 'val': headline}})
     return i_pre_meta, field_dict
+
 
 def parse_post_meta(i_pre_meta, article_lines):
     fields = ['URL:', 'GRAPHIC:', 'LANGUAGE:', 'DOCUMENT-TYPE:', 'PUBLICATION-TYPE:', 'SUBJECT:', 'PERSON:', 'CITY:', 'STATE:', 'COUNTRY:']
@@ -60,49 +61,111 @@ def parse_post_meta(i_pre_meta, article_lines):
     i_text = i_pre_meta + 2
     text = article_lines[i_text:min(field_dict.values(), key=lambda field: field['idx'] - 2)['idx']]
     text = ' '.join([line if line != '' else '\n' for line in text])
-    field_dict.update({'TEXT': {'idx': i_text, 'val': text}})
+    field_dict.update({'TEXT:': {'idx': i_text, 'val': text}})
     return field_dict
 
 
-def get_article_sections(article):
+def get_article_field_dict(article):
     article_lines = article.splitlines()
-    i_date, date = get_date(article_lines)
-    if i_date == len(article_lines) - 1:
+    field_dict = get_date(article_lines)
+    if field_dict['DATE:']['idx'] == len(article_lines) - 1:
         return None
-    i_pre_meta, pre_meta_dict = parse_pre_meta(i_date, article_lines)
+    i_pre_meta, pre_meta_dict = parse_pre_meta(field_dict['DATE:']['idx'], article_lines)
     post_meta_dict = parse_post_meta(i_pre_meta, article_lines)
     pre_meta_dict.update(post_meta_dict)
-    return pre_meta_dict
+    field_dict.update(pre_meta_dict)
+    return field_dict
+
+# Connect to the database
 
 
-def get_field_dict(text):
+def insert_articles(field_dicts):
+    with connection.cursor() as cursor:
+        # Create a new record
+        for field_dict in field_dicts:
+            field_dict['DATE:']['val'] = field_dict['DATE:']['val'].strftime('%Y-%m-%d %H:%M:%S')
+            field_dict['LENGTH:']['val'] = ''.join(filter(str.isdigit, field_dict['LENGTH:']['val']))
+            keys = '`, `'.join(['A_' +k[:-1].replace('-','_') for k in field_dict.keys()])
+            vals = [str(v['val']) for v in field_dict.values()]
+            sql = "INSERT INTO `articles` (`" + keys + "`) VALUES (" + "%s, " * (len(vals) - 1) + " %s)"
+            cursor.execute(sql, vals)
+    connection.commit()
+
+
+
+def get_source_field_dicts(source, text):
     articles = split_articles(text)
     field_dicts = []
     for article in articles:
-        field_dict = get_article_sections(article)
-        if field_dict is not None:
+        field_dict = OrderedDict([('SOURCE:', {'idx': -1, 'val': source})])
+        article_field_dict = get_article_field_dict(article)
+        if article_field_dict is not None:
+            field_dict.update(article_field_dict)
             field_dicts.append(field_dict)
-    return {k: [field_dict[k]['val'] for field_dict in field_dicts] for k in field_dicts[0].keys()}
+    return field_dicts
 
-sources = [d for d in listdir('sources/') if isdir('sources/' + d)]
-print(sources)
-for source in sources:
-    print(source)
-    source_path = 'sources/' + source + '/'
-    files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
-    write_header = True
 
-    for f_path in files:
-        with open(source_path + f_path, "r", encoding="utf8") as f:
-            field_dict = get_field_dict(f.read())
-        print(f_path)
+def read_sources(write_csv=False, write_mysql=False):
+    sources = [d for d in listdir('sources/') if isdir('sources/' + d)]
+    print(sources)
+    for source in sources:
+        print(source)
+        source_path = 'sources/' + source + '/'
+        files = [f for f in listdir(source_path) if isfile(join(source_path, f))]
+        write_header = True
+        for f_path in files:
+            with open(source_path + f_path, "r", encoding="utf8") as f:
+                field_dicts = get_source_field_dicts(source, f.read())
+            print(f_path + " LEN: " + str(len(field_dicts)))
 
-        if write_header:
-            with open(source + ".csv", "w") as outfile:
-                writer = csv.writer(outfile)
-                writer.writerow(list(field_dict.keys()))
-            write_header = False
+            if write_mysql:
+                insert_articles(field_dicts)
 
-        with open(source + ".csv", "a") as outfile:
-            writer = csv.writer(outfile)
-            writer.writerows(zip(*field_dict.values()))
+            if write_csv:
+                field_dict = {k: [field_dict[k]['val'] for field_dict in field_dicts] for k in field_dicts[0].keys()}
+                if write_header:
+                    with open(source + ".csv", "w") as outfile:
+                        writer = csv.writer(outfile)
+                        writer.writerow(list(field_dict.keys()))
+                    write_header = False
+
+                with open(source + ".csv", "a") as outfile:
+                    writer = csv.writer(outfile)
+                    writer.writerows(zip(*field_dict.values()))
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='bias_in_news',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+read_sources(write_mysql=True)
+
+connection.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
